dba517ce69895b21c0178ed9fd4ed81165ea99d4
Jinn <115912358+Jinnlk@users.noreply.github.com>
2023-12-23 16:41:08 -0800

    basic display and rendering

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("hello world")
-print("second line")
+import pygame
+
+WIDTH, HEIGHT = 1280, 720
+GAME_OVER = False
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+
+
+def check_time():
+    return pygame.time.get_ticks()
+
+
+while not GAME_OVER:
+
+    screen.fill("purple")
+    pygame.display.flip()
+
+    if check_time() > 5000:
+        GAME_OVER = True
+
+pygame.quit()
